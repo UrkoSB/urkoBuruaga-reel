94b75efbe8b900490ac40fae45c2522f4a3eb5ac
Urko <smith_riau@hotmail.com>
2024-05-16 01:27:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title>Urko Buruaga | Live looping & acoustic</title>
+    <meta name="description" content="Gasteizko kantautorea. Kontzertuetan live looping-a eta akustikoa uztartzen ditut. | Cantautor gasteiztarra. Cantautor y live looping artist.">
+</head>
+<body>
+    <div id="container">
+        <?php include_once('parts/header.php') ?>
+        
+        
+        <aside>ASIDE</aside>
+        <main>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus nulla nec rutrum porta. Maecenas vitae magna diam. Morbi efficitur, felis et pulvinar varius, erat purus sollicitudin ipsum, vitae accumsan est nulla a metus. Aliquam sodales, mi eget tempor ullamcorper, orci est consectetur nunc, nec mattis justo odio nec sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac justo vitae ipsum accumsan eleifend. Fusce luctus malesuada massa et pellentesque.</p<>
+            <p>Nulla at nunc id quam dignissim efficitur a vitae mauris. Etiam dapibus, erat id porttitor auctor, orci ex tempus orci, nec pharetra ante turpis non velit. Ut ut varius lectus. Duis lobortis, dolor iaculis venenatis ultrices, felis velit efficitur tellus, molestie sagittis libero eros sed nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean gravida vestibulum vehicula. Vestibulum consectetur dolor ac ornare hendrerit. Phasellus laoreet scelerisque ligula sit amet ultricies. Proin lobortis tellus leo.</p>
+            <p>Vivamus sagittis nibh et purus convallis convallis. Donec et dui vitae ante consectetur venenatis. Maecenas fermentum risus nec nunc convallis accumsan. Nullam nec libero nibh. Aliquam efficitur vel massa in semper. Pellentesque tempor non lorem eu porttitor. Fusce rhoncus odio nec ex placerat viverra. Vestibulum ultricies ullamcorper posuere. Vestibulum tortor nisi, pretium volutpat ante quis, commodo luctus mi.</p>
+            <p>Proin a metus sagittis, fermentum felis sit amet, scelerisque justo. Cras id diam enim. Integer in sodales neque, pharetra semper nisl. Donec ornare venenatis eros eu accumsan. Duis id sapien dapibus, condimentum urna a, scelerisque orci. Vestibulum id pharetra massa. Phasellus mollis quis purus a commodo. Suspendisse eu laoreet libero. Integer ante orci, finibus et erat a, semper bibendum turpis. Nullam id pharetra libero, quis mollis nunc. Aliquam enim augue, luctus placerat dui sit amet, dignissim finibus enim. Proin non neque volutpat, tempor arcu quis, pulvinar est. Maecenas finibus sapien sit amet dui sagittis, in vehicula urna tempor. Etiam ut velit non dolor condimentum sollicitudin et vitae magna.</p>
+            <p>Curabitur vel lobortis libero, vitae molestie purus. Nunc et hendrerit felis. Donec id auctor tellus, eget efficitur quam. Phasellus fringilla lacinia elit, in convallis urna euismod non. Vivamus posuere risus sit amet turpis tempor, faucibus ullamcorper turpis consectetur. Donec lobortis, enim non sodales luctus, nisi ipsum malesuada dolor, euismod tincidunt sem eros vitae massa. Proin sed suscipit massa, non facilisis odio.</p>
+        </main>
+        
+        <footer>FOOTER</footer>
+    </div>
+    <?php include_once('parts/nav.php') ?>
+</body>
+</html>
---
+++ parts/header.php
@@ -0,0 +1,13 @@
+<header>
+    <h1>Live looping & acoustic songwriter</h1>
+    <section id="logo">
+        <img src="assets/img/urko-buruaga-logo.png" alt="">
+        <p id="site-title">Urko Buruaga</p>
+    </section>
+    <section id="rrss">
+    <?php include_once('assets/img/svg/rrss/instagram.svg') ?>
+    <?php include_once('assets/img/svg/rrss/youtube.svg') ?>
+    <?php include_once('assets/img/svg/rrss/tiktok.svg') ?>
+    </section>
+    <p id="scroll-indicator">⌵</p>
+</header>
