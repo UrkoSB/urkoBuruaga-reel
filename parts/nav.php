a63e6426859d440bf5a0807bdfdc0924f0bfc161
Urko <smith_riau@hotmail.com>
2024-05-29 01:16:50 +0200

    Añadir enlaces a secciones 'nor naiz' y 'kontratazioa'

--- parts/nav.php
+++ parts/nav.php
@@ -4,7 +4,7 @@
             <p class="nav-item-eus">Nor naiz</p>
             <p class="nav-item-cas">Quién soy</p>
         </a>
-        <a href="" id="nav-kontratazioa" class="nav-simplify">
+        <a href="#section_kontratazioa" id="nav-kontratazioa" class="nav-simplify">
             <p class="nav-item-eus">Kontratazioa</p>
             <p class="nav-item-cas">Contratación</p>
             <?php include_once('assets/img/svg/icons/envelope.svg') ?>
