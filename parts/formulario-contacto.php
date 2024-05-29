b7e6f23a573151e7209efdb7dc25221afdbd5a65
Urko <smith_riau@hotmail.com>
2024-05-29 19:59:59 +0200

    Incluir y maquetar formulario de contacto

---
+++ parts/formulario-contacto.php
@@ -0,0 +1,26 @@
+<form action="" method="post" id="contact-form">
+    <label for="izena">Izena | Nombre</label>
+    <input type="text" name="izena" id="izena" required>
+
+    <label for="email">E-mail</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="mezua">Mezua | Mensaje</label>
+    <textarea name="mezua" id="mezua" required></textarea>
+
+    <button type="submit">
+        Bidali
+        <svg xmlns="http://www.w3.org/2000/svg" width="34.963" height="34.963" viewBox="0 0 34.963 34.963">
+            <path id="Trazado_3" data-name="Trazado 3" d="M27.937,1.2A.875.875,0,0,0,26.8.063L1.343,10.246h0l-.791.315a.875.875,0,0,0-.143,1.552l.717.455,0,0,8.741,5.561,5.561,8.741,0,0,.455.717a.875.875,0,0,0,1.55-.145ZM24.729,4.508,11.615,17.622l-.376-.591a.875.875,0,0,0-.269-.269l-.591-.376L23.492,3.271l2.061-.824Z" transform="translate(8.187 0) rotate(17)" fill="rgba(255,255,255,0.8)"/>
+        </svg>
+        Enviar
+    </button>
+
+    <p id="lopd-form">
+    Zure datu pertsonalen tratamenduari buruzko informazioa: Arduraduna:  Urko Sáenz de Buruaga. Helburua: harreman-inprimakiaren bitartez helarazitako mezuak kudeatzea. Eskubideak eta informazio gehigarria: Zure datuen tratamenduari buruzko eskubideak erabili daitezke eta urkoburuaga.com webguneko pribatutasun politikan aipatu tratamenduari buruzko informazio gehigarria lor daiteke. 
+    </p>
+    <p id="lopd-form">
+    Información relativa al tratamiento de sus datos personales: Responsable: Urko Sáenz de Buruaga. Finalidad: Gestionar los mensajes enviados a través de este formulario. Derechos e información adicional: Pueden ejercerse los derechos relativos al tratamiento de sus datos y obtener información adicional sobre dicho tratamiento en la política de privacidad del sitio web urkoburuaga.com.
+    </p>
+
+</form>
--- parts/sections/kontratazioa.php
+++ parts/sections/kontratazioa.php
@@ -23,11 +23,7 @@
                 <p>~ <span class="laburdura">g.g.b.</span><span class="ez-laburdura">Gutxi gorabehera</span>~</p>
             </span>
         </span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-stopwatch" viewBox="0 0 16 16">
-            <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z"/>
-            <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64l.012-.013.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
-        </svg>
-
+        <?php include('assets/img/svg/icons/kronometro.svg') ?>
         <span>
             <h3>Duración</h3>
             <span class="multiparrafo">
@@ -42,11 +38,7 @@
             <h3>Nire ekipoa</h3>
             <p>Nire PA sistema erabiltzeko aukera.</p>
         </span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-speaker" viewBox="0 0 16 16">
-            <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-            <path d="M8 4.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5M8 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 3a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m-3.5 1.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-        </svg>
-
+        <?php include('assets/img/svg/icons/pa.svg') ?>
         <span>
             <h3>Equipo propio</h3>
             <p>Opción de usar mi propio sistema de PA.</p>
@@ -57,22 +49,20 @@
         <span>
             <h3>Faktura</h3>
             <p>Faktura egiteko aukera.</p>
-        </span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
-            <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
-            <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
-        </svg>
-
+        </span>    
+        <?php include('assets/img/svg/icons/faktura.svg') ?>
         <span>
             <h3>Factura</h3>
             <p>Posibilidad de hacer factura.</p>
         </span>
     </article>
 
-    <article id="contact-form">
-
-        <h3>Harremanetan jarri</h3>
-        <h3>Ponte en contacto</h3>
+    <article id="article-harremana">
+        <h3>Harremana</h3>
+        <?php include('assets/img/svg/icons/envelope.svg') ?>
+        <h3>Contacto</h3>
+        <?php include('parts/formulario-contacto.php') ?>
 
     </article>
+
 </section>
