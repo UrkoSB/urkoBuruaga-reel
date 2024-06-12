5f96a73f87c77c66f654a4d2e81272080b5003b8
Urko <smith_riau@hotmail.com>
2024-06-12 02:39:07 +0200

    Cambiar lang

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once('parts/functionality/message-sent-controller.php');
 include_once('../ignore/keys.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="eu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
