cc7ebabc7286c272a1c85cfc2d730af3dddaa8e9
Urko <smith_riau@hotmail.com>
2024-06-14 12:45:50 +0200

    Añadir defer a carga del script propio

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include_once('../ignore/conf.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <script src="js/scripts.js"></script>
+    <script defer src="js/scripts.js"></script>
     <title>Urko Buruaga | Live looping & acoustic</title>
     <meta name="description" content="Gasteizko kantautorea. Kontzertuetan live looping-a eta akustikoa uztartzen ditut. | Cantautor gasteiztarra. Live looping artist.">
 </head>
